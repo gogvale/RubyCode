0a13a744eecfacda4dbb9a72e0f6644903109171
Gabriel Vale <gogvale2011@gmail.com>
2020-07-14 18:11:34 -0500

    add spacing

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,7 @@ def bubble_sort(arr)
   last_idx = arr.length - 1
   while (last_idx > 0)
     swaped = false
-    0.upto(last_idx-1).each do |idx|
+    0.upto(last_idx - 1).each do |idx|
       if arr[idx] > arr[idx + 1]
         arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
         swaped = true
