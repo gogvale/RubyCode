f5c4050b5185e2a29806f1684f6db58bc41fc7f9
Gabriel Vale <gogvale2011@gmail.com>
2020-07-14 17:55:19 -0500

    check if already sorted

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,24 @@
 def bubble_sort(arr)
   last_idx = arr.length - 1
   while (last_idx > 0)
+    swaped = false
     arr.each_with_index do |i, idx|
       if idx == last_idx
         break
       else
         if i > arr[idx + 1]
           arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx] #swap
+          swaped = true
         end
       end
+    end
+    if not swaped
+      break
     end
     last_idx -= 1
   end
   return arr
 end
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
 # => [0,2,2,3,4,78]
