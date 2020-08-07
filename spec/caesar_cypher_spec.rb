1d84b8b56678cb9a7178c68d4bbd62ccf93fd0cb
Gabriel Vale <gogvale2011@gmail.com>
2020-08-06 20:55:39 -0500

    change method to #caesar_cypher

--- spec/caesar_cypher_spec.rb
+++ spec/caesar_cypher_spec.rb
@@ -1,24 +1,24 @@
 require './caesar_cypher'
 
-describe '#replace_char' do
+describe '#caesar_cipher' do
   it 'simple replacement within range' do
-    expect(replace_char('a', 2)).to eql('c')
+    expect(caesar_cipher('abcde', 2)).to eql('cdefg')
   end
   it 'from z goes to a' do
-    expect(replace_char('z', 2)).to eql('b')
+    expect(caesar_cipher('wxyz', 2)).to eql('yzab')
   end
 
   it 'works with uppercase' do
-    expect(replace_char('Z', 2)).to eql('B')
+    expect(caesar_cipher('WXyZ', 2)).to eql('YZaB')
   end
 
   it 'works with HUGE numbers' do
-    expect(replace_char('a', 26_000_000)).to eql('a')
+    expect(caesar_cipher('Hello, World', 26_000_000)).to eql('Hello, World')
   end
   it 'works with negative numbers' do
-    expect(replace_char('a', -1)).to eql('z')
+    expect(caesar_cipher('Uryyb, Jbeyq!', -13)).to eql('Hello, World!')
   end
   it 'works with 0' do
-    expect(replace_char('F', 0)).to eql('F')
+    expect(caesar_cipher('Goodbye, World', 0)).to eql('Goodbye, World')
   end
 end
